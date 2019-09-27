cdb1fbdad254bdc4664f6863843b03ae02aae412
Kamome Shido <shidokamo@users.noreply.github.com>
2019-09-27 04:46:01 +0000

    Kafka のコードのエラーを修正

--- sbt/src/main/scala/org/example/WordCountKafka.scala
+++ sbt/src/main/scala/org/example/WordCountKafka.scala
@@ -4,13 +4,15 @@ import java.util.Properties
 import org.apache.flink.api.scala._
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer010,FlinkKafkaProducer010}
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+// import org.apache.flink.streaming.util.serialization.{JsonDeserializationSchema,SimpleStringSchema}
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 
 object WordCountKafka {
   def main(args: Array[String]): Unit = {
 
-
     // set up the execution environment
-    val env = ExecutionEnvironment.getExecutionEnvironment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment()
 
     val params: ParameterTool = ParameterTool.fromArgs(args)
     // make parameters available in the web interface
@@ -21,10 +23,11 @@ object WordCountKafka {
     properties.setProperty("bootstrap.servers", params.get("broker"))
     properties.setProperty("group.id", "test")
 
+    // Kafka consumer and start position
+    val consumer = new FlinkKafkaConsumer010[String]("test-topic", new SimpleStringSchema(), properties)
+
     // Kafka connector with schema to deserialize the data
-    val stream = env
-                .addSource(new FlinkKafkaConsumer010[String]("test-topic", new SimpleStringSchema(), properties))
-                .print()
+    val stream = env .addSource(consumer)
 
     // Kafka Start position
     stream.setStartFromLatest()
