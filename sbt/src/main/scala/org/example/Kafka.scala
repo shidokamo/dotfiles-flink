43a3e50fe19016f5a90a425f828316cf999ff7a9
Kamome Shido <shidokamo@users.noreply.github.com>
2019-10-02 03:51:27 +0000

    Window 処理をデバッグ中

--- sbt/src/main/scala/org/example/Kafka.scala
+++ sbt/src/main/scala/org/example/Kafka.scala
@@ -2,7 +2,12 @@ package org.example
 
 import java.util.Properties
 import java.util.concurrent.TimeUnit
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
 import com.fasterxml.jackson.databind.node.ObjectNode
+import scala.util.parsing.json.JSONObject
 import org.apache.flink.api.scala._
 import org.apache.flink.api.java.utils.ParameterTool
 // import org.apache.flink.streaming.util.serialization.JSONDeserializationSchema // 廃止されたっぽい
@@ -12,14 +17,22 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010
 import org.apache.flink.streaming.api.scala.DataStream
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
 
 object Kafka {
+  val fmt = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
   def main(args: Array[String]): Unit = {
 
     // set up the execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    // Event Time
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+
+    // Parameters
     val params: ParameterTool = ParameterTool.fromArgs(args)
     // make parameters available in the web interface
     env.getConfig.setGlobalJobParameters(params)
@@ -51,14 +64,26 @@ object Kafka {
         }
         .keyBy(0)
 //        .timeWindow(Time.of(2500, TimeUnit.MILLISECONDS), Time.of(500, TimeUnit.MILLISECONDS)) // Should be after Key
-        .timeWindow(Time.of(2500, TimeUnit.MILLISECONDS))
-        .minBy(0)
+//        .timeWindow(Time.of(2500, TimeUnit.MILLISECONDS))
+//        .countWindow(5, 1)
+        .window(SlidingEventTimeWindows.of(Time.seconds(5), Time.seconds(1)))
+        .min(1)
+        .map { v =>
+          println(v)
+          v
+          // val zdt = new Date(v.time).toInstant().atZone(ZoneId.systemDefault())
+          // val time = fmt.format(zdt)
+          // val json = Map("time" -> time, "bid" -> v.bid, "min" -> v.min)
+          // val retval = JSONObject(json).toString()
+          // println(retval)
+          // retval
+        }
 //      .sum(1)
 //      .addSink(publisher)
 //      .name("kafka")
 
     // execute and print result
-    data.print()
+    // data.print()
     // counts.writeAsCsv(params.get("output"), "\n", " ")
 
     env.execute("Flink Scala Kafka Word Count Example")
